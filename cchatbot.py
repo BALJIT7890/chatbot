95e062bcaf8fb51dcbcc70f8cd9f7ac2ebba67db
BALJIT7890 <154889672+BALJIT7890@users.noreply.github.com>
2024-01-15 11:01:08 +0000

    Add files via upload

---
+++ cchatbot.py
@@ -0,0 +1,162 @@
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+import re
+import tkinter as tk
+from tkinter import scrolledtext
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+for intent in intents["intents"]:
+    intent["patterns"] = [pattern.lower() for pattern in intent["patterns"]]
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.keras')
+
+#Clean up the sentences
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+#Convert the sentences into a bag of words
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence) #bow: Bag Of Words, feed the data into the neural network
+    res = model.predict(np.array([bow]))[0] #res: result. [0] as index 0
+
+    ERROR_THRESHOLD = 0.5
+    results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    # Use the flag to debugging
+    if decrease_error_threshold:
+        while results == [] and ERROR_THRESHOLD > 0.1:
+            ERROR_THRESHOLD -= 0.01
+            results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    try:
+        tag = intents_list[0]['intent']
+        list_of_intents = intents_json['intents']
+        for i in list_of_intents:
+            if i['tag'] == tag:
+                result = random.choice(i['responses'])
+                break
+    # Own addition. Handle an exception
+    except IndexError:
+        result = "I don't know. Please rephrase your question"
+    # end of Own addition
+    return result
+
+print("COM727 Chatbot is here!")
+
+# Own addition. Logs all chatbot history
+from datetime import datetime
+import socket
+computer_name = socket.gethostname()
+file_name = f"{computer_name}_chat_log.txt"
+
+# Load replacement.json to change underscored words from dataset (readme.md)
+json_file = 'replacements.json'
+# Load the JSON file as a dictionary
+with open(json_file, 'r') as file:
+    replacements = json.load(file)
+
+
+def replace_words(text, replacements):
+    for key, value in replacements.items():
+        pattern = r'\b' + re.escape(key) + r'\b'
+        text = re.sub(pattern, value, text)
+    return text
+
+def send_response():
+    try:
+        message = user_entry.get()
+        if message.lower() == 'exit':
+            root.quit()
+        else:
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            num_words = len(message.split())
+            if num_words >= 3 or message.lower() in ["hello", "hi", "hey", "hello!", "hi!", "hey!"]:
+                message = replace_words(message, replacements)
+                ints = predict_class(message)
+                response = get_response(ints, intents)
+                if show_details:
+                    response = response + "          " + json.dumps(ints)
+            else:
+                response = "Your question is too short, can you restate it?"
+            log_data = {
+                "timestamp": timestamp,
+                "input_message": message,
+                "output_response": response
+            }
+
+            # Display conversation in the text widget
+            conversation_text = f"{timestamp} - You: {message}\n com727: {response}\n\n"
+            conversation.insert(tk.END, conversation_text)
+
+            # Save conversation log to a file
+            if show_details:
+                with open(file_name, 'a') as log_file:
+                    log_file.write(json.dumps(log_data) + '\n')
+
+            # Clear the message entry
+            user_entry.delete(0, tk.END)
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+# Create the main window or root window
+root = tk.Tk()
+root.title("World Cup 2022")
+
+# Entry field where the user can type messages
+user_entry = tk.Entry(root, width=90)
+user_entry.grid(row=1, column=0, padx=12, pady=12)
+
+# Create button Send
+send_button = tk.Button(root, text="SEND", command=send_response, font=("Helvetica", 14))
+send_button.grid(row=1, column=1, padx=12, pady=12)
+
+user_entry.focus()
+# Enter button invoke sending the message
+user_entry.bind('<Return>', lambda event=None: send_response())
+
+# Create a scrolled text widget to display the conversation
+conversation = scrolledtext.ScrolledText(root, width=90, height=20, wrap=tk.WORD)
+conversation.grid(row=0, column=0, padx=12, pady=12, columnspan=2)
+
+# Create the greeting message
+greeting = "Welcome to Coding Avengers! Ask me about World Cup 2022"
+conversation.insert(tk.END, f" com727: {greeting}\n\n")
+conversation.see(tk.END)
+
+# Set 'show_details' to True if you want to display additional details
+show_details = False
+# Set 'decrease_error_threshold' to True if you want to debug predicting below Threshold
+decrease_error_threshold = False
+
+# Run the interface
+root.mainloop()
---
+++ training.py
@@ -0,0 +1,111 @@
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+#from keras.layers import Conv1D, GlobalMaxPooling1D
+#from keras.layers import LSTM
+from keras.optimizers.legacy import SGD
+
+# from tensorflow.keras.optimizer import SGD
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+
+# Own addition 1. Lower case
+for intent in intents["intents"]:
+    intent["patterns"] = [pattern.lower() for pattern in intent["patterns"]]
+# end of own  addition 1
+
+# Own addition 2. Create a dictionary for words with underscore (readme.md)
+def find_and_create_replacements(data, key_intents, key_patterns):
+    replacements = {}
+    for intent in data[key_intents]:
+        # Iterate through the patterns in each intent
+        for pattern in intent[key_patterns]:
+            words = pattern.split()  # Split the pattern into words
+            # Process the words in the pattern
+            for word in words:
+                # Check if the word contains underscores
+                if '_' in word:
+                    # Split the word into parts by underscores
+                    parts = word.split('_')
+                    replacements[f"{parts[0]} {parts[1]}"] = word
+    return replacements
+
+
+replacements = find_and_create_replacements(data=intents, key_intents='intents', key_patterns='patterns')
+output_file = 'replacements.json'
+# Save the replacements to the JSON file
+with open(output_file, 'w') as file:
+    json.dump(replacements, file, indent=4)
+# end of Own addition 2
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', '/', '@']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        pos_tags = nltk.pos_tag(word_list)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+stop_words = set(stopwords.words('english'))
+# Own addition 3. Lower case
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
+# end of Own addition 3
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training, dtype=object)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+#model.add(Conv1D(128, 5, activation='relu', input_shape=(len(train_x[0]), 1)))
+#model.add(GlobalMaxPooling1D())
+#model.add(LSTM(128, input_shape=(len(train_x[0]), 1), activation='relu'))
+
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+# model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.keras', hist)
+print('Done')
